9700b82b73837671aaaff996ca5b0315fb77b1cd
souf <arhlabi.soufiane02@gmail.com>
2024-04-27 01:37:28 +0800

    fix small typo

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORLD,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 db.connect();
